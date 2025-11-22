617b94aab3d695fe81628b36908d49a47c73347d
ivanfurlan1 <66447232+ivanfurlan1@users.noreply.github.com>
2025-11-22 01:28:01 -0300

    Add files via upload

---
+++ starlinks_live.js
@@ -0,0 +1,11 @@
+// Archivo de actualización remota para la App
+// Edita esto en GitHub y la App lo descargará al iniciar.
+
+window.LIVE_STARLINKS_UPDATE = [
+    {
+        tle: `STARLINK-G6-78 STACK    
+1 72000C 25268A   25325.19760162  .00031689  00000+0  49963-4 0    05
+2 72000  42.9953 252.3581 0009783 290.5047  87.5187 16.02760423    11`
+    },
+    // Podés agregar más bloques aquí si hay múltiples lanzamientos
+];
